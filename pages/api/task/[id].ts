d5797aa71c18013e26f529bd9b436d50ce4a9338
Yiğitcan <yigitarc45@gmail.com>
2023-01-09 05:26:53 +0300

    fs removed

--- pages/api/task/[id].ts
+++ pages/api/task/[id].ts
@@ -1,31 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import fs from 'fs';
 import { Task } from '../../../types/taskType';
 import { ResponseApi } from '../../../types/responseType';
-import { json } from 'stream/consumers';
-import path from 'path';
+import { Tasks, setTasks } from '.';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseApi<Task | {}>>) {
   return new Promise<void>((resolve, reject) => {
-    const jsonDirectory = process.cwd() + '/tasks.json';
-
-    const { query } = req;
-    const { id } = query as { id: string };
-
-    fs.readFile(jsonDirectory, 'utf8', (err, jsonString) => {
-      let tasks: Task[] = JSON.parse(jsonString);
-
-      if (err) {
-        res.status(500).json({
-          message: 'Something is wrong',
-          status: 500,
-          data: [],
-        });
-        reject();
-      }
+    try {
+      const { query } = req;
+      const { id } = query as { id: string };
 
       if (req.method === 'GET') {
-        const selectedTask = tasks.find((task) => task.id === id) || ({} as Task);
+        const selectedTask = Tasks.find((task) => task.id === id) || ({} as Task);
         if (selectedTask) {
           res.status(200).json({
             message: 'Data fetched',
@@ -43,52 +28,33 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Respon
       } else if (req.method === 'POST') {
         const task = JSON.parse(req.body);
 
-        if (tasks.find((_task) => _task.id === task.id)) {
-          tasks = tasks.map((_task) => (_task.id === task.id ? task : _task));
+        if (Tasks.find((_task) => _task.id === task.id)) {
+          setTasks(Tasks.map((_task) => (_task.id === task.id ? task : _task)));
         } else {
-          tasks.push(task);
+          Tasks.push(task);
         }
 
-        fs.writeFile(jsonDirectory, JSON.stringify(tasks), 'utf8', (err) => {
-          if (err) {
-            res.status(500).json({
-              message: 'Something is wrong',
-              status: 500,
-              data: [],
-            });
-            reject();
-          }
-          res.status(201).json({
-            message: `Task is created`,
-            status: 201,
-            data: task,
+        res.status(201).json({
+          message: `Task is created`,
+          status: 201,
+          data: task,
+        });
+
+        resolve();
+      } else if (req.method === 'DELETE') {
+        const selectedTask = Tasks.find((task) => task.id === id);
+        if (selectedTask) {
+          const filteredTasks = Tasks.filter((task) => task.id !== id);
+
+          setTasks(filteredTasks);
+
+          res.status(200).json({
+            message: `Task is deleted`,
+            status: 200,
+            data: selectedTask,
           });
 
           resolve();
-        });
-      } else if (req.method === 'DELETE') {
-        const selectedTask = tasks.find((task) => task.id === id);
-        if (selectedTask) {
-          const filteredTask = tasks.filter((task) => task.id !== id);
-
-          fs.writeFile(jsonDirectory, JSON.stringify(filteredTask), 'utf8', (err) => {
-            if (err) {
-              res.status(500).json({
-                message: 'Something is wrong',
-                status: 500,
-                data: [],
-              });
-              reject();
-            }
-
-            res.status(200).json({
-              message: `Task is deleted`,
-              status: 200,
-              data: selectedTask,
-            });
-
-            resolve();
-          });
         } else {
           res.status(404).json({
             message: `Task with id: ${id} not found.`,
@@ -98,6 +64,13 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Respon
           reject();
         }
       }
-    });
+    } catch (error) {
+      res.status(500).json({
+        message: 'Something is wrong',
+        status: 500,
+        data: [],
+      });
+      reject();
+    }
   });
 }
--- pages/api/task/index.ts
+++ pages/api/task/index.ts
@@ -1,21 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import fs from 'fs';
 import { ResponseApi } from '@/types/responseType';
 import { Task } from '@/types/taskType';
 
+export let Tasks = [] as Task[];
+
+export const setTasks = (tasks: Task[]) => {
+  Tasks = tasks;
+};
+
 export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseApi<Task[]>>) {
   return new Promise<void>((resolve, reject) => {
-    const jsonDirectory = process.cwd() + '/tasks.json';
-
-    fs.readFile(jsonDirectory, 'utf8', (err, jsonString) => {
-      if (err) {
-        res.status(500).json({ message: 'Internal Server Error', status: 500, data: [] });
-        reject();
-      }
-
-      const tasks: Task[] = JSON.parse(jsonString);
-      res.status(200).json({ message: 'Data fetched', status: 200, data: tasks });
+    try {
+      res.status(200).json({ message: 'Data fetched', status: 200, data: Tasks });
       resolve();
-    });
+    } catch (error) {
+      res.status(500).json({ message: 'Internal Server Error', status: 500, data: [] });
+      reject();
+    }
   });
 }
