1e10c63c2b6c6eb26d8626217d814a1c1edb81a0
Yiğitcan <yigitarc45@gmail.com>
2023-01-09 05:17:55 +0300

    meta tag update

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ function Layout({ children }: Props) {
   return (
     <html lang="tr" className={fontFamily.className}>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1" />
         <link rel="stylesheet" href="https://storage.googleapis.com/non-spec-apps/mio-icons/latest/outline.css" />
       </Head>
       <body className="container">
